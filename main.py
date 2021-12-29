c4c6069f4e1ec48047228cba162beeac4c75d8cf
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2021-12-29 04:02:53 +0300

    Fixed .gitignore

--- main.py
+++ main.py
@@ -14,42 +14,38 @@ app.config.from_object(__name__)
 @app.route('/heroes/image/<cardID>', methods=['GET'])
 def get_hero_image_data(cardID):
     filename="heroes_art/%s.png" % cardID.lower()
+    tempfilename = 'temp_heroes.png'
     try:
         pngfile = Image.open(filename)
+        im_crop = pngfile.crop((42, 107, 488, 490))
+        im_crop.save(tempfilename, quality=95)
     except Exception as e:
         return flask.make_response('Dude, why you asked me about <b>%s</b>? Idk this buddy..' % str(cardID), 404)
-
-    im_crop = pngfile.crop((42, 107, 488, 490))
-    tempfilename='temp_heroes.png'
-    im_crop.save(tempfilename, quality=95)
-    return  send_file(tempfilename, mimetype='image/gif')
+    return send_file(tempfilename, mimetype='image/gif')
 
 @app.route('/cards/image/<cardID>', methods=['GET'])
 def get_cards_image_data(cardID):
     filename="cards_art/%s.png" % cardID.lower()
+    tempfilename = 'temp_card.png'
     try:
         pngfile = Image.open(filename)
+        im_crop = pngfile.crop((28, 36, 470, 695))
+        im_crop.save(tempfilename, quality=95)
     except Exception as e:
         return flask.make_response('Dude, why you asked me about <b>%s</b>? Idk this buddy..' % str(cardID), 404)
-
-    im_crop = pngfile.crop((28, 36, 470, 695))
-    tempfilename='temp_card.png'
-    im_crop.save(tempfilename, quality=95)
-    return  send_file(tempfilename, mimetype='image/gif')
+    return send_file(tempfilename, mimetype='image/gif')
 
 @app.route('/hero_power/image/<cardID>', methods=['GET'])
 def get_hero_power_image_data(cardID):
     filename="heroes_art/%s.png" % cardID.lower()
+    tempfilename = 'temp_hp.png'
     try:
         pngfile = Image.open(filename)
+        im_crop = pngfile.crop((35, 47, 476, 704))
+        im_crop.save(tempfilename, quality=95)
     except Exception as e:
         return flask.make_response('Dude, why you asked me about <b>%s</b>? Idk this buddy..' % str(cardID), 404)
 
-    im_crop = pngfile.crop((35, 47, 476, 704))
-    tempfilename='temp_hp.png'
-    im_crop.save(tempfilename, quality=95)
-    pngfile = Image.open(tempfilename)
-    print(pngfile.size, pngfile.format)
     return  send_file(tempfilename, mimetype='image/gif')
 
 
